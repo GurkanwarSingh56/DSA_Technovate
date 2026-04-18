f49c3b48da8d20287cf69e9905f199928640e71d
Gauravkatnoria12 <144324982+Gauravkatnoria12@users.noreply.github.com>
2026-04-18 10:25:26 +0530

    Problem 1 solved

--- Problems/Problem1.cpp
+++ Problems/Problem1.cpp
@@ -2,21 +2,12 @@
 #include <vector>
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
+void func(int n){
     vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cout << "Enter element " << i + 1 << ": ";
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
-    cout << "Elements: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
 
     int max = arr[0];
     for (int i = 0; i < n; i++) {
@@ -24,18 +15,19 @@ int main() {
             max = arr[i];
         }
     }
+    
     cout << "Maximum element: " << max << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    func(n);
     return 0;
 }
 
-
-
-// Output of Program :
-// Enter the number of elements: 5
-// Enter element 1: 6
-// Enter element 2: 4
-// Enter element 3: 2
-// Enter element 4: 8
-// Enter element 5: 1
-// Elements: 6 4 2 8 1 
+// Output :
+// Enter the number of elements: 5 
+// Enter elements: 8 5 3 7 2 
 // Maximum element: 8
