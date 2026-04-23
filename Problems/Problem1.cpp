9023dd42cc51452c9410f157f63b127613cff21c
Gauravkatnoria12 <144324982+Gauravkatnoria12@users.noreply.github.com>
2026-04-23 17:39:40 +0530

    Problem 1 Solved

--- Problems/Problem1.cpp
+++ Problems/Problem1.cpp
@@ -1,33 +1,58 @@
+/*
+Problem Statement:
+Find the maximum element in an array.
+
+Input:
+First line: integer n (size of array)
+Second line: n space-separated integers
+
+Output:
+Print the maximum element in the array
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
-void func(int n){
-    vector<int> arr(n);
-    cout << "Enter elements: ";
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    int max = arr[0];
-    for (int i = 0; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
+// Function to find maximum element in array
+int findMaxElement(const vector<int>& arr) {
+    int maxElement = arr[0];
+    
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
         }
     }
     
-    cout << "Maximum element: " << max << endl;
+    return maxElement;
 }
 
 int main() {
     int n;
-    cout << "Enter the number of elements: ";
     cin >> n;
-    func(n);
+
+    // Edge case: if array is empty
+    if (n == 0) {
+        return 0;
+    }
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int result = findMaxElement(arr);
+    cout << result;
+
     return 0;
 }
 
-// Output :
-// Enter the number of elements: 5 
-// Enter elements: 8 5 3 7 2 
-// Maximum element: 8
+
+/*
+Input:
+5
+15 10 3 24 20
+Output:
+24
+*/
