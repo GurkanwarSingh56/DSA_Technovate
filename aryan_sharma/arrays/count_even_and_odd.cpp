705679a371d2f127279f0db08f36cffde573b78a
Aryan Sharma <aryansharma462007@gmail.com>
2026-05-31 14:39:40 +0530

    Added comments for count even and odd numbers.

--- aryan_sharma/arrays/count_even_and_odd.cpp
+++ aryan_sharma/arrays/count_even_and_odd.cpp
@@ -1,7 +1,14 @@
+// Problem Statement 4
+// Title: Count Even and Odd Numbers
+// Description:
+// Given an array, count how many even and odd numbers are present.
+ 
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
+// function to count even and odd numbers
 void countEvenOdd(vector<int> &arr){
     int evenCount = 0, oddCount = 0;
     for(int i = 0; i<arr.size(); i++){
@@ -12,20 +19,24 @@ void countEvenOdd(vector<int> &arr){
             oddCount++;
         }
     }
+
+    // output of result
     cout<<"Even : "<< evenCount<<endl;
     cout<<"Odd : "<< oddCount<<endl;
 }
 
 int main(){
-
+    
     int n;
     cin>>n;
-
+    
+    // Input array elements
     vector<int> arr(n);
     for(int i = 0; i<n; i++){
         cin>>arr[i];
     }
 
+    // Function call
     countEvenOdd(arr);
 
     return 0;
