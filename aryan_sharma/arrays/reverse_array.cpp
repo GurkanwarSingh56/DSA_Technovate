47e3ee8ae8f9c24309a90e71643ed6fe0ba7e5a5
Aryan Sharma <aryansharma462007@gmail.com>
2026-06-08 10:52:52 +0530

    Added comments

--- aryan_sharma/arrays/reverse_array.cpp
+++ aryan_sharma/arrays/reverse_array.cpp
@@ -9,6 +9,7 @@ Write a program to reverse the elements of an array.
 #include<vector>
 using namespace std;
 
+//Function to reverse an array
 void reverseArray(vector<int>&arr){
     int start = 0;
     int end = arr.size() - 1;
@@ -28,12 +29,16 @@ int main(){
     int n;
     cin>>n;
 
+// Input array elements
     vector<int>arr(n);
     for(int i = 0; i<n; i++){
         cin>>arr[i];
     }
-
+    
+// Function call
     reverseArray(arr);
+
+// Printing the reversed array
     for(int i = 0; i<n; i++){
         cout<<arr[i]<<" ";
     }
