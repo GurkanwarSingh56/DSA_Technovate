fad7f63c38a204e1d2a647cc48d33bebbed96e61
Gurkanwar Singh <157824528+GurkanwarSingh56@users.noreply.github.com>
2026-06-13 18:21:53 +0530

    added basics

---
+++ BasicsOfC/basic1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a,c;   
+   int b =scanf("%d %d",&a,&c);
+   printf("%d",b); 
+}
---
+++ BasicsOfC/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int i=5;
+   int a = 5<5&&++i;
+   printf("%d",i);
+}
